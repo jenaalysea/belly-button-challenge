50d97de329a3963fd2d4ae55b4561dde69994045
jenaalysea <147357960+jenaalysea@users.noreply.github.com>
2024-02-05 13:08:15 -0700

    Add files via upload
    updated code for the bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -12,13 +12,22 @@ var otuCounts = data.samples.find(entry => entry.id == sample);
 var result = otuCounts.otu_values.slice(0, 10);
 
 //******************************creating the vertical bar chart***********************************************
- 
+
+let sortedById = searchResults.sort((a, b) => {
+  if (a.ID > b.ID){
+    return -1} 
+    else{
+      return 0
+    }
+
+  }); 
+
+
   // Trace1 for the OTU Data
   var trace1 = {
     x: result,
     y: otuCounts.otu_ids.slice(0, 10).map(id => `OTU ${id}`),
-    text: otuCounts.otu_labels.slice(0, 10),
-    type: "bar",
+       type: "bar",
     orientation: "h"
   };
   
@@ -39,91 +48,7 @@ var result = otuCounts.otu_values.slice(0, 10);
 
 // ******************************create a bubble data chart***********************************************
 
-function buildCharts(sample) {
-  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
 
-// step 1, get metadata from data
-var sample_values = data.metadata.find(entry => entry.id == sample);
-// step 2, filter
-var otuCounts = data.samples.find(entry => entry.id == sample);
-    
-
-    // Trace1 for the OTU Data (Bubble Chart)
-    var trace1 = {
-      x: result.map(object => object.ID),
-      y: result.map(object => object.otuId),
-      mode: 'markers',
-      marker: {
-        color: 'Viridis',
-        opacity: [1, 0.8, 0.6, 0.4],
-        size: result.map(object => object.sample_values),
-      },
-    };  
-
-    // Data array
-    var data = [trace1];
-
-    // Apply a title to the layout
-    var layout = {
-      title: "OTU Bubble Chart",
-      xaxis: {
-        title: 'OTU ID'
-      },
-      yaxis: {
-        title: 'Sample Values'
-      }
-    };
-
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("plot", plotlyData, layout);
-  });
-}
-
-// ***********************create a view of sample data**************************************
-
-// Function to fetch data and initialize the page
-function init() {
-  // Fetch the JSON data
-  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-    // Populate the dropdown with Test Subject IDs
-    const dropdown = d3.select("#selDataset");
-
-    data.names.forEach((id) => {
-      dropdown.append("option").text(id).property("value", id);
-    });
-
-    // Initialize the page with the first ID
-    const initialId = data.names[0];
-    optionChanged(initialId);
-  });
-}
-
-// Do dropdown changes
-function optionChanged(selectedId) {
-  // Fetch the data again (for more details or additional data if needed)
-  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-    // Update demographic information based on the selected ID
-    const metadata = data.metadata.find((entry) => entry.id == selectedId);
-    updateDemographicInfo(metadata);
-  });
-}
-
-// Update demographic information
-function updateDemographicInfo(metadata) {
-  // Select the panel body where demographic info will be displayed
-  const panelBody = d3.select("#sample-metadata");
-
-  // Clear content
-  panelBody.html("");
-
-  // Shwo info
-  Object.entries(metadata).forEach(([key, value]) => {
-    panelBody.append("p").text(`${key}: ${value}`);
-  });
-}
-
-// Initialize the page
-init();
 
 // ***********************create the gauge chart**************************************
 /* <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
